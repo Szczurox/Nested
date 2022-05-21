69c67aedf09ecb46288e2b3e27c22dbb002653ef
Szczurox <58273015+Szczurox@users.noreply.github.com>
2022-05-21 14:18:32 +0200

    small update
    Changed:
    - the way scroll is handled by ChatMain
    
    Added:
    - messages now have username of the user who sent them

--- src/components/chat/ChatMain.tsx
+++ src/components/chat/ChatMain.tsx
@@ -1,5 +1,5 @@
 import { TextareaAutosize } from "@material-ui/core";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ChatHeader from "./ChatHeader";
 import Message from "./Message";
 import UploadFile from "./UploadFile";
@@ -22,71 +22,47 @@ import {
 import moment from "moment";
 import { serverTimestamp } from "firebase/firestore";
 import { useChannel } from "context/channelContext";
+import { useUser } from "context/userContext";
 
 export interface MessageData {
   id: string;
   content: string;
   timestamp: string;
+  uid: string;
 }
 
 export const ChatMain: React.FC = ({}) => {
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState<MessageData[]>([]);
-  const [renderViewScroll, setRenderViewScroll] = useState(0);
-  const [renderView, setRenderView] = useState(50);
   const [lastKey, setLastKey] = useState<Timestamp>(new Timestamp(0, 0));
-  const [messagesLoading, setMessagesLoading] = useState(false);
+  const [scrollAfterLoad, setScrollAfterLoad] = useState(false);
   const [messagesEnd, setMessagesEnd] = useState(false);
+  const listInnerRef = useRef<HTMLHeadingElement>(null);
+
+  const { channel } = useChannel();
+  const { user } = useUser();
 
   const app = createFirebaseApp();
   const db = getFirestore(app);
-  const { channel } = useChannel();
 
-  const fetchMorePosts = (key: any) => {
-    if (lastKey!.seconds != undefined) {
-      setMessagesLoading(true);
-      getMessages(key);
-      setMessagesLoading(false);
+  const handleScroll = (e: any) => {
+    if (listInnerRef.current) {
+      const { scrollTop, scrollHeight, clientHeight } = listInnerRef.current;
+      console.log(scrollHeight, scrollTop, clientHeight);
+      if (scrollTop < 100 && !messagesEnd) {
+        getMessages();
+      }
     }
   };
 
-  function getMessagesFirstBatch() {
-    // Channels query
-    const qMes = query(
-      collection(
-        db,
-        "groups",
-        "H8cO2zBjCyJYsmM4g5fv",
-        "categories",
-        channel.idC,
-        "channels",
-        channel.id,
-        "messages"
-      ),
-      orderBy("createdAt", "desc"),
-      limit(20)
-    );
+  const scrollToBottom = () => {
+    if (listInnerRef.current != null) {
+      listInnerRef.current.scrollTop =
+        listInnerRef.current.scrollHeight - listInnerRef.current.clientHeight;
+    }
+  };
 
-    const unsub = onSnapshot(qMes, (querySnapshot) => {
-      setMessages(
-        querySnapshot.docs.reverse().map((doc) => ({
-          id: doc.id,
-          content: doc.data().content,
-          timestamp: doc.data().time,
-        }))
-      );
-      if (querySnapshot.docs.length > 0) {
-        setLastKey(
-          querySnapshot.docs[querySnapshot.docs.length - 1].data().createdAt
-        );
-        setMessagesEnd(false);
-      } else setMessagesEnd(true);
-    });
-
-    return unsub;
-  }
-
-  function getMessages(key: string) {
+  function getMessages() {
     // Channels query
     const qMes = query(
       collection(
@@ -101,7 +77,7 @@ export const ChatMain: React.FC = ({}) => {
       ),
       orderBy("createdAt", "desc"),
       limit(20),
-      startAfter(key)
+      startAfter(lastKey)
     );
 
     const unsub = onSnapshot(qMes, (querySnapshot) => {
@@ -110,6 +86,7 @@ export const ChatMain: React.FC = ({}) => {
           id: doc.id,
           content: doc.data().content,
           timestamp: doc.data().time,
+          uid: doc.data().userid,
         })),
         ...messages,
       ]);
@@ -126,9 +103,52 @@ export const ChatMain: React.FC = ({}) => {
   }
 
   useEffect(() => {
+    function getMessagesFirstBatch() {
+      // Channels query
+      const qMes = query(
+        collection(
+          db,
+          "groups",
+          "H8cO2zBjCyJYsmM4g5fv",
+          "categories",
+          channel.idC,
+          "channels",
+          channel.id,
+          "messages"
+        ),
+        orderBy("createdAt", "desc"),
+        limit(20)
+      );
+
+      const unsub = onSnapshot(qMes, (querySnapshot) => {
+        setMessages(
+          querySnapshot.docs.reverse().map((doc) => ({
+            id: doc.id,
+            content: doc.data().content,
+            timestamp: doc.data().time,
+            uid: doc.data().userid,
+          }))
+        );
+        if (querySnapshot.docs.length > 0) {
+          setLastKey(
+            querySnapshot.docs[querySnapshot.docs.length - 1].data().createdAt
+          );
+          setMessagesEnd(false);
+        } else setMessagesEnd(true);
+
+        if (listInnerRef.current) {
+          console.log("U MAD?");
+          listInnerRef.current.focus();
+          scrollToBottom();
+        }
+      });
+
+      return unsub;
+    }
+
     const unsub = getMessagesFirstBatch();
     return () => unsub();
-  }, [channel.id, getMessagesFirstBatch]);
+  }, [channel.id]);
 
   async function sendMessage(e: any) {
     // Send Message
@@ -150,6 +170,7 @@ export const ChatMain: React.FC = ({}) => {
             createdAt: serverTimestamp(),
             time: moment().utcOffset("+00:00").format(),
             content: input,
+            userid: user.uid,
           }
         );
       }
@@ -160,18 +181,19 @@ export const ChatMain: React.FC = ({}) => {
   return (
     <div className={styles.chat}>
       <ChatHeader />
-      <div style={{ textAlign: "center" }}>
-        {messagesLoading ? (
-          <p>Loading..</p>
-        ) : !messagesEnd ? (
-          <button onClick={() => fetchMorePosts(lastKey)}>More Messages</button>
-        ) : (
-          <span>You are up to date!</span>
-        )}
-      </div>
-      <div className={styles.chat_messages}>
-        {messages.map(({ id, content, timestamp }) => (
-          <Message key={id} id={id} content={content} timestamp={timestamp} />
+      <div
+        className={styles.chat_messages}
+        onScroll={(e) => handleScroll(e)}
+        ref={listInnerRef}
+      >
+        {messages.map(({ id, content, timestamp, uid }) => (
+          <Message
+            key={id}
+            id={id}
+            content={content}
+            time={timestamp}
+            userid={uid}
+          />
         ))}
       </div>
       <div className={styles.chat_input}>
--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Avatar, TextareaAutosize } from "@material-ui/core";
 import style from "../../styles/components/chat/Message.module.scss";
+import moment from "moment";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
+import { createFirebaseApp } from "../../firebase/clientApp";
 
 interface MessageProps {
   id: string;
   content: string;
-  username?: string;
-  timestamp?: string;
+  userid: string;
+  time?: string;
 }
 
 export const Message: React.FC<MessageProps> = ({
   id,
   content,
-  timestamp,
-  username = "Username",
+  time,
+  userid = "Username",
 }) => {
+  const [username, setUsername] = useState("");
+
+  const app = createFirebaseApp();
+  const db = getFirestore(app);
+
+  useEffect(() => {
+    async function getUserData() {
+      const docSnap = await getDoc(doc(db, "profile", userid));
+      if (docSnap.exists()) setUsername(docSnap.data().username);
+    }
+    getUserData();
+  });
+
   return (
     <div className={style.message} id={id}>
       <div className={style.message_info}>
@@ -26,7 +42,9 @@ export const Message: React.FC<MessageProps> = ({
         </div>
         <h4>
           {username}
-          <span className={style.message_timestamp}>{timestamp}</span>
+          <span className={style.message_timestamp}>
+            {moment(time).local().format("MMMM Do YYYY [at] hh:mm:ss a")}
+          </span>
         </h4>
         <div className="message_content">
           <p>{content}</p>
